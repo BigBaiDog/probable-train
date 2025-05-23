2b3486fbda2200fcc3fba3b344669cfafa6998b7
yuan <1194423126@qq.com>
2025-05-23 20:20:22 +0800

    测试邮件发送

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,27 @@ const app = new Hono()
 
 app.get('/', (c) => {
   const transporter = nodemailer.createTransport({
-  host: 'smtp.yourserver.com',
-  port: 587,
-  auth: {
-    user: 'myid@yourserver.com',
-    pass: 'my-password'
-  },
-});
+    host: 'smtp.163.com',
+    port: 465,
+    auth: {
+      user: 'bigbaidog@163.com',
+      pass: 'TSq7LMCK6QFGgsH2'
+    },
+  });
+  const mailOptions = {
+    from: '"Node.js Mailer" <bigbaidog@163.com>', // 发件人
+    to: '1194423126@qq.com',                     // 收件人
+    subject: '测试邮件',                                // 邮件主题
+    text: '这是一封测试邮件的正文内容。',                  // 纯文本正文
+    html: '<p>这是一封 <b>测试邮件</b> 的 HTML 正文内容。</p>', // HTML 正文
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return c.text('邮件发送失败：'+error);
+    }
+    return c.text('邮件发送成功：'+info.messageId);
+  });
+
   return c.text('Hello Hono!')
 })
 
