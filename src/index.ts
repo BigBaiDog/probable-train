c09290b894cc282cd630b4469b1f929dc26912c9
yuan <1194423126@qq.com>
2025-05-23 20:29:55 +0800

    111

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import nodemailer from 'nodemailer';
 const app = new Hono()
 
-app.get('/'， (c) => {
+app.get('/', (c) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.163.com',
     port: 465,
