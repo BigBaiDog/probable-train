542b766dad7b414d19554e2b90a9f3e07cfa6c64
yuan <1194423126@qq.com>
2025-05-23 21:37:57 +0800

    123

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,42 @@ import { Hono } from 'hono'
 import nodemailer from 'nodemailer';
 const app = new Hono()
 
-app.get('/', (c) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.163.com',
-secure: true,
-    port: 465,
-    auth: {
-      user: 'bigbaidog@163.com',
-      pass: 'TSq7LMCK6QFGgsH2'
-    },
-  });
-  const mailOptions = {
-    from: '"Node.js Mailer" <bigbaidog@163.com>', // 发件人
-    to: '1194423126@qq.com',                     // 收件人
-    subject: '测试邮件',                                // 邮件主题
-    text: '这是一封测试邮件的正文内容。',                  // 纯文本正文
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return c.text('邮件发送失败：'+error);
-    }
-    return c.text('邮件发送成功：'+info.messageId);
-  });
+app.get('/', async(c) => {
 
-  return c.text('Hello Hono!')
+const transporter = nodemailer.createTransport({
+    // host: 'smtp.163.com',       // 163 邮箱 SMTP 服务器
+    // service: '163',
+     host: '123.58.180.7', // smtp.163.com的实际IP
+    port: 465,                  // SSL 端口
+    secure: true,               // 使用 SSL
+    auth: {
+        user: 'bigbaidog@163.com',  // 你的 163 邮箱地址
+        pass: 'TSq7LMCK6QFGgsH2'       // 前面获取的授权码
+    }
+});
+// 发送邮件的函数
+async function sendMail() {
+    try {
+        const info = await transporter.sendMail({
+            from: '"博主小助手" <your_email@163.com>', // 发件人显示名和邮箱
+            to: '1194423126@qq.com',               // 收件人邮箱
+            subject: '来自 Node.js 的问候',             // 邮件主题
+            text: '你好！这是一封用 Node.js 发送的测试邮件！', // 纯文本内容
+            // html: '<p>你好！这是一封用 <b>Node.js</b> 发送的测试邮件！</p>' // HTML 内容
+        });
+        console.log('邮件发送成功:', info.response);
+  return c.text('1')
+    } catch (error) {
+        console.log('邮件发送失败:', error);
+  return c.text('2')
+    }
+}
+
+// 执行发送
+ await sendMail();
+
+
+  // return c.text('123')
 })
 
 export default app
