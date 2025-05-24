ef97cd60f0f9002f5f1b1e1064b4f06ea90b5837
yuan <1194423126@qq.com>
2025-05-24 20:35:22 +0800

    111

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,39 @@ import { Hono } from 'hono'
 import nodemailer from 'nodemailer';
 const app = new Hono()
 
-app.get('/', async (c) => {
-
-    const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "maddison53@ethereal.email",
-            pass: "jn7jnAPss4f63QBp6D",
-        },
-    });
-
-    // Wrap in an async IIFE so we can use await.
+app.get('/', async(c) => {
 
 
+  const transporter = nodemailer.createTransport({
+    host: "2408:8756:3af0:3009::123",
+    port: 465,
+    // secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: '1194423126@qq.com',
+      pass: 'papytfjwhpxzigba'
+    },
+
+  });
+
+  async function sendMail() {
     try {
-            (async () => {
         const info = await transporter.sendMail({
-            from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
-            to: "1194423126@qq.com",
-            subject: "Hello ✔",
-            text: "Hello world?", // plain‑text body
+            from: '"博主小助手" <your_email@163.com>', // 发件人显示名和邮箱
+            to: 'bigbaidog@163.com',               // 收件人邮箱
+            subject: '来自 Node.js 的问候',             // 邮件主题
+            text: '你好！这是一封用 Node.js 发送的测试邮件！', // 纯文本内容
+            // html: '<p>你好！这是一封用 <b>Node.js</b> 发送的测试邮件！</p>' // HTML 内容
         });
-        console.log("Message sent: %s", info.messageId);
-        return c.text("Message sent: " + info.messageId);
-    })();
+        console.log('邮件发送成功:', info.response);
+  return c.text('1')
     } catch (error) {
         console.log('邮件发送失败:', error);
   return c.text('2')
     }
+}
+
+// 执行发送
+ await sendMail();
 
 })
 
