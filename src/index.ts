249c86b60143554b7c97ee22dc90745a2146aa35
yuan <1194423126@qq.com>
2025-05-24 21:41:16 +0800

    163

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ app.get('/', async(c) => {
 
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.qq.com",
+    host: "smtp.163.com",
     port: 465,
     // secure: false, // upgrade later with STARTTLS
     auth: {
-      user: '1194423126@qq.com',
-      pass: 'papytfjwhpxzigba'
+      user: 'bigbaidog@163.com',
+      pass: 'TSq7LMCK6QFGgsH2'
     },
 
   });
@@ -19,8 +19,8 @@ app.get('/', async(c) => {
   async function sendMail() {
     try {
         const info = await transporter.sendMail({
-            from: '1194423126@qq.com', // 发件人显示名和邮箱
-            to: 'bigbaidog@163.com',               // 收件人邮箱
+            from: 'bigbaidog@163.com', // 发件人显示名和邮箱
+            to: '1194423126@qq.com',               // 收件人邮箱
             subject: '来自 Node.js 的问候',             // 邮件主题
             text: '你好！这是一封用 Node.js 发送的测试邮件！', // 纯文本内容
             // html: '<p>你好！这是一封用 <b>Node.js</b> 发送的测试邮件！</p>' // HTML 内容
